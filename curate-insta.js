8d644c8c9d7db5c7397c3c268ef2ecfbb8f358f9
Getzel Rubashkin <getzel@journeymanweb.com>
2018-08-30 12:00:14 -0400

    added TamperMonkey tags

--- curate-insta.js
+++ curate-insta.js
@@ -1,8 +1,19 @@
+// ==UserScript==
+// @name         Instagram Curator
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Hide locally blacklisted images from a browser-based Instagram timeline
+// @author       Getzel Rubashkin
+// @match        https://www.instagram.com/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-3.3.1.min.js
+// ==/UserScript==
+
 (function() {
     'use strict';
     //Add Toggle to Header
     //Trigger curator on DOMChange
-      //Review posts (<article>, flagging reviewed
+      //Review posts (<article>), flagging reviewed
       //For non-reviewed
         //Add "hide/show" toggle
         //is blacklisted?
