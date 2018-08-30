43272385ce58ed85b6548923517bd560c7be0b81
Getzel Rubashkin <getzel@journeymanweb.com>
2018-08-30 11:52:56 -0400

    Create base file

---
+++ curate-insta.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+    //Add Toggle to Header
+    //Trigger curator on DOMChange
+      //Review posts (<article>, flagging reviewed
+      //For non-reviewed
+        //Add "hide/show" toggle
+        //is blacklisted?
+          //add "flagged" class
+        
+        
+   jQuery(function($){
+      //page toggle click
+        //show/hide flagged articles
+        //add "hide/show" class to body
+     
+     //article toggle click
+        //add/remove "flagged" class
+        //add/remove from local storage blacklist
+        //if body.hasClass hide, hide
+   });
+})();
